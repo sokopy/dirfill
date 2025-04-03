cad30baaf3a439036e086170f9fe4670b47f5475
sokopy <gabrojaslopez@gmail.com>
2025-04-02 20:29:52 -0600

    Made code a bit cleaner and made a dangerous one

--- main.py
+++ main.py
@@ -19,25 +19,16 @@ while True:
     else:
         print(f"'{confirm}' is not a valid option. Type 'y' to execute or 'n' to exit")
         time.sleep(3)
-def get_random_unicode(length):
-    get_char = chr
-    include_ranges = [
-        ( 0x00AE, 0x00FF ),
-        ( 0x0100, 0x017F ),
-        ( 0x0180, 0x024F ),
-        ( 0x2C60, 0x2C7F ),
-        ( 0x16A0, 0x16F0 ),
-        ( 0x0370, 0x0377 ),
-        ( 0x037A, 0x037E ),
-        ( 0x0384, 0x038A ),
-        ( 0x038C, 0x038C ),
-    ]
+
+def random_unicode(l):
+    include_ranges = [(0x00AE, 0x00FF), (0x0100,0x017F), (0x0180,0x024F), (0x2C60,0x2C7F), (0x16A0,0x16F0), 
+    (0x0370,0x0377), (0x037A,0x037E), (0x0384,0x038A),(0x038C,0x038C),]
     alphabet = [
-        get_char(code_point) for current_range in include_ranges
+        chr(code_point) for current_range in include_ranges
             for code_point in range(current_range[0], current_range[1])
     ]
-    return ''.join(random.choice(alphabet) for i in range(length))
+    return ''.join(random.choice(alphabet) for i in range(l))
 
 for i in range(65000):
     for i in range(65000):
-        open(f"{get_random_unicode(250)}", "x", encoding="utf-8").write(str(get_random_unicode(65000)))
+        open(f"{random_unicode(250)}", "x", encoding="utf-8").write(str(random_unicode(65000)))
