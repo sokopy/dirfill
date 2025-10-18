e9c868f96a47a70158e71030ab4437f24595d66a
sokopy <gabrojaslopez@gmail.com>
2025-10-18 13:14:40 -0600

    Little error woops

--- fuck/main.py
+++ fuck/main.py
@@ -1,9 +1,9 @@
 import random
 
-def random_unicode():
+def random_unicode(l):
     include_ranges = [(0x00AE,0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
     ab = [chr(code_point) for current_range in include_ranges for code_point in range(current_range[0], current_range[1])]
-    return ''.join(random.choice(ab) for _ in range(65536))
+    return ''.join(random.choice(ab) for _ in range(l))
 
 while 1:
-    open(f"{random_unicode(250)}", "x", encoding="utf-8").write(str(random_unicode()))
+    open(f"{random_unicode(250)}", "x", encoding="utf-8").write(str(random_unicode(65536)))
--- main.py
+++ main.py
@@ -10,10 +10,10 @@ while 1:
         print(f"'{conf}' is not a valid option. Type 'y' to execute or 'n' to exit")
         time.sleep(3)
 
-def random_char():
+def random_char(l):
     include = [(0x00AE, 0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
     ab = [chr(code_point) for current_range in include for code_point in range(current_range[0], current_range[1])]
-    return ''.join(random.choice(ab) for _ in range(65536))
+    return ''.join(random.choice(ab) for _ in range(l))
 
 while 1:
-    open(f"{random_char(250)}", "x", encoding="utf-8").write(str(random_char()))
+    open(f"{random_char(250)}", "x", encoding="utf-8").write(str(random_char(65536)))
