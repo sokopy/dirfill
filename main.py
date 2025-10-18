49a9c51506efd9e541a2627630f1c9bd3c828034
sokopy <gabrojaslopez@gmail.com>
2025-10-18 12:36:30 -0600

    I like this number more

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ while 1:
 def random_char():
     include = [(0x00AE, 0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
     ab = [chr(code_point) for current_range in include for code_point in range(current_range[0], current_range[1])]
-    return ''.join(random.choice(ab) for _ in range(65000))
+    return ''.join(random.choice(ab) for _ in range(65536))
 
 while 1:
     open(f"{random_char(250)}", "x", encoding="utf-8").write(str(random_char()))
