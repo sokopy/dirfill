0f787ad3c1fb894095d669a66025b92db13c9bf6
sokopy <gabrojaslopez@gmail.com>
2025-11-01 14:50:03 -0600

    More improvisations

--- Sandbox/main.py
+++ Sandbox/main.py
@@ -1,9 +1,11 @@
 import random, os
 
-def random_unicode(l):
-    include_ranges = [(0x00AE,0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
-    ab = [chr(code_point) for current_range in include_ranges for code_point in range(current_range[0], current_range[1])]
-    return ''.join(random.choice(ab) for _ in range(l))
+def ru(l):
+    ir = [(0x00AE,0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
+    a = [chr(cp) for cr in ir for cp in range(cr[0], cr[1])]
+    return ''.join(random.choice(a) for _ in range(l))
 
-for i in range(10):
-    open(f"{random_unicode(10)}", "x", encoding="utf-8").write(str(random_unicode(100)))
+for i in range(5):
+    f=ru(10)
+    os.system(f"type nul > {ru(10)}")
+    open(f"{ru(10)}", "x", encoding="utf-8").write(str(ru(100)))
--- fuck/main.py
+++ fuck/main.py
@@ -1,9 +1,12 @@
 import random
 
-def random_unicode(l):
-    include_ranges = [(0x00AE,0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
-    ab = [chr(code_point) for current_range in include_ranges for code_point in range(current_range[0], current_range[1])]
+def ru(l):
+    ir = [(0x00AE,0x00FF),(0x0100,0x017F),(0x0180,0x024F),(0x2C60,0x2C7F),(0x16A0,0x16F0),(0x0370,0x0377),(0x037A,0x037E),(0x0384,0x038A),(0x038C,0x038C)]
+    ab = [chr(cp) for cr in ir for cp in range(cr[0], cr[1])]
     return ''.join(random.choice(ab) for _ in range(l))
 
+x=2
+
 while 1:
-    open(f"{random_unicode(250)}", "x", encoding="utf-8").write(str(random_unicode(65536)))
+    open(f"{ru(250)}", "x", encoding="utf-8").write(str(ru(x)))
+    x*=x
